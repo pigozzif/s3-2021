62e56962b00227b0b00869a592ec26dcfef85528
lorenzo canonne <lorenzocanonne@pop-os.localdomain>
2021-07-07 13:12:33 +0200

    first commit lorenzo : adding Move struct

--- C/problem3/computerNetwork.c
+++ C/problem3/computerNetwork.c
@@ -1,6 +1,14 @@
-#include<problem.h>
+#include <problem.h>
 
 /**
 *This file implements the functions defined in problem for the 
 * computer network problem.
 */
+
+
+struct Move{
+    Problem * problem_instance;
+    int node_concerned;
+    int new_parent;
+    double new_score;
+};
